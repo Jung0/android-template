54401e43f1cb0986a40096a8e20a9d678c9f3a54
Jung0 <awf.15.92@gmail.com>
2018-03-28 08:52:34 +0900

    ♻️ Change to abstract class

--- app/src/main/java/com/example/djung0/android_template/AppFragment.kt
+++ app/src/main/java/com/example/djung0/android_template/AppFragment.kt
@@ -5,7 +5,7 @@ import android.support.v4.app.Fragment
 import com.example.djung0.android_template.shared.di.Injectable
 import javax.inject.Inject
 
-class AppFragment : Fragment(), Injectable {
+abstract class AppFragment : Fragment(), Injectable {
     @Inject
     lateinit var viewModel: ViewModelProvider.Factory
 }
