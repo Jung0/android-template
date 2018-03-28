90610ec0352951c78811a5b0bdec723c5db60081
Jung0 <awf.15.92@gmail.com>
2018-03-28 21:40:06 +0900

    ♻️ Fix format

--- app/src/main/java/com/example/djung0/android_template/AppActivity.kt
+++ app/src/main/java/com/example/djung0/android_template/AppActivity.kt
@@ -1,5 +1,6 @@
 package com.example.djung0.android_template
 
+import android.arch.lifecycle.ViewModelProvider
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import com.example.djung0.android_template.shared.di.Injectable
@@ -9,8 +10,10 @@ import dagger.android.support.HasSupportFragmentInjector
 import javax.inject.Inject
 
 abstract class AppActivity : AppCompatActivity(), HasSupportFragmentInjector, Injectable {
-//    @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
-    @Inject lateinit var androidInjetor: DispatchingAndroidInjector<android.support.v4.app.Fragment>
+//    @Inject
+//    lateinit var viewModelFactory: ViewModelProvider.Factory
+    @Inject
+    lateinit var androidInjetor: DispatchingAndroidInjector<android.support.v4.app.Fragment>
 
     override fun supportFragmentInjector(): AndroidInjector<Fragment> = androidInjetor
 }
