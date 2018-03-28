5f57103fbf332b153c5c12a0926b5b515df7d4c2
Jung0 <awf.15.92@gmail.com>
2018-03-28 21:44:02 +0900

    ✨ Add bindViewModelFactory

--- app/src/main/java/com/example/djung0/android_template/shared/di/module/ViewModelModule.kt
+++ app/src/main/java/com/example/djung0/android_template/shared/di/module/ViewModelModule.kt
@@ -1,7 +1,13 @@
 package com.example.djung0.android_template.shared.di.module
 
+import android.arch.lifecycle.ViewModelProvider
+import com.example.djung0.android_template.shared.di.ViewModelFactory
+import dagger.Binds
 import dagger.Module
 
 @Module
 internal abstract class ViewModelModule {
+
+    @Binds
+    abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
 }
