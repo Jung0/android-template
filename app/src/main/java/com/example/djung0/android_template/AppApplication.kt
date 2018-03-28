c17b54f7b85412436265275b88375c7f4a4f147d
Jung0 <awf.15.92@gmail.com>
2018-03-28 21:41:25 +0900

    ♻️ small fix

--- app/src/main/java/com/example/djung0/android_template/AppApplication.kt
+++ app/src/main/java/com/example/djung0/android_template/AppApplication.kt
@@ -6,17 +6,18 @@ import com.example.djung0.android_template.shared.di.module.ApiModule
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 
-class AppApplication : DaggerApplication() {
 
-    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
-        return DaggerAppComponent.builder()
-                .application(this)
-                .apiModule(ApiModule(this.getString(R.string.domain_api)))
-                .build()
-    }
+class AppApplication : DaggerApplication() {
 
     override fun onCreate() {
         super.onCreate()
         setupActivityLifecycle()
+    }
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerAppComponent.builder()
+                .application(this)
+                .apiModule(ApiModule(getString(R.string.domain_api)))
+                .build()
     }
 }
